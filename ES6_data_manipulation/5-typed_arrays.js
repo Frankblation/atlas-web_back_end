df4771b72687f911f80e715b12a2b461b0cae276
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-01-10 19:05:54 -0600

    less the 0

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
-export default function createInt8TypedArray9(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  if (position < 0 || position >= length) {
-    throw TypeError('Position is outside of range');
+export default function createInt8TypedArray(length, position, value) {
+    const buffer = new ArrayBuffer(length);
+    const int8Array = new Int8Array(buffer);
+    if (position >= 0 && position < length) {
+      int8Array[position] = value;
+    } else {
+      throw new Error('Position outside range');
+    }
+    return new DataView(buffer);
   }
-  view[position] = value;
-  return buffer;
-}
