7cae00dcd004c0e95123acd5109e083d58d40271
Frankblation <frankblation@gmail.com>
2024-05-02 14:55:28 -0500

    corrected!

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+   concat's two strings.
+
+   Args:
+       str1 (str): The first srting.
+       str2 (str): The second string.
+
+   Returns:
+       string: str1 and or str2.
+   """
+    return str1 + str2
