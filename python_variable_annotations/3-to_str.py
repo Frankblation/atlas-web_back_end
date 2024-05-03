0f3cf847eb03b7887ac1be5539cca740cd589f09
Frankblation <frankblation@gmail.com>
2024-05-03 13:25:14 -0500

    3 done

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""to_str takes a float n returns the string"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str function
+    
+    argument: n is a float
+
+    returns: float as a string
+
+    """
+    return str(n)
