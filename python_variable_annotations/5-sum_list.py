5bd5b12b498d6b9ab2c146c4ed8853f8a3aa97d0
Frankblation <frankblation@gmail.com>
2024-05-03 14:20:50 -0500

    this is crazy

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+"""
+This script defines a function to calculate the sum of elements in a list.
+"""
+
+
+def sum_list(input_list: list = float) -> sum:
+    """
+    Calculate the sum of elements in a list.
+
+    Args:
+        input_list (list): A list of floats.
+
+    Returns:
+        float: The sum of elements in the input list.
+    """
+    return sum(input_list)
