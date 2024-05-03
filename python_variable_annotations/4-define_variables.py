93daffa1b12d9c9f3f405e8c33ed09aec3b7ba18
Frankblation <frankblation@gmail.com>
2024-05-03 14:10:35 -0500

    i'm doing good i think

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Define variables with specified values and annotations.
+
+Variables:
+    a (int): An integer with a value of 1.
+    pi (float): A float with a value of 3.14.
+    i_understand_annotations (bool): A boolean with a value of True.
+    school (str): A string with a value of "Holberton".
+"""
+
+a: int = 1  # An integer with a value of 1
+pi: float = 3.14  # A float with a value of 3.14
+i_understand_annotations: bool = True  # A boolean with a value of True
+school: str = "Holberton"  # A string with a value of "Holberton"
