68e9fdebb01cd84417ff559039517db8176cac9d
Frankblation <frankblation@gmail.com>
2024-05-06 13:34:50 -0500

    fixing pycode

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This script defines a function that converts a key-value pair into a tuple.
+This script defines a function that converts 
+a key-value pair into a tuple.
 """
 
 from typing import Union, Tuple
@@ -8,7 +9,8 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple where the first element is the string k and the second element
+    Returns a tuple where the first element 
+    is the string k and the second element
     is the square of the int/float v.
 
     Args:
@@ -16,6 +18,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v (Union[int, float]): The integer or float value.
 
     Returns:
-        Tuple[str, float]: A tuple containing the string k and the square of v as a float.
+        Tuple[str, float]: A tuple containing the 
+        string k and the square of v as a float.
     """
     return (k, float(v) ** 2)
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -14,7 +14,9 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
         multiplier (float): The multiplier to be used.
 
     Returns:
-        Callable[[float], float]: A function that takes a float and returns the result of multiplying it by the multiplier.
+        Callable[[float], float]: A function 
+        that takes a float and returns the result 
+        of multiplying it by the multiplier.
     """
     def multiplier_function(x: float) -> float:
         return x * multiplier
