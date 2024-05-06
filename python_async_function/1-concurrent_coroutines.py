5bfc321aadd7b85c25ba56e7ad3fca6772582c57
Frankblation <frankblation@gmail.com>
2024-05-06 14:00:47 -0500

    wait_n with a random wait

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronous coroutine that waits for a
+    random delay between 0 and max_delay seconds.
+
+    Args:
+        max_delay (int): The maximum delay in seconds (default is 10).
+
+    Returns:
+        float: The random delay waited.
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous routine that spawns wait_random n times with the specified max_delay.
+
+    Args:
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List[float]: A list of all the delays in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return sorted(await asyncio.gather(*tasks))
