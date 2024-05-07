10734a24f6879fef691305f0d970ac4bd6beded3
Frankblation <frankblation@gmail.com>
2024-05-06 21:06:43 -0500

    idk why it wasn't working

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,11 @@ Asynchronous generator that yields random numbers between 0 and 10.
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+
+async def async_generator() -> Generator[float, None, None]:
     """
     Coroutine that loops 10 times, waiting asynchronously for 1 second
     each time, then yields a random number between 0 and 10.
@@ -18,12 +20,3 @@ async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-asyncio.run(print_yielded_values())
