0cdc0e862d69fee0bc7ea00a25bab77c85bef501
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-10 17:57:51 -0500

    what changed

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python3
-"""This module provides a Server class to paginate a dataset of popular baby names.
-It includes the following features:
-
-1. `index_range(page, page_size)`: Returns a tuple of start and end indices for pagination.
-2. `Server` class:
-   - `dataset()`: Loads and caches the dataset from a CSV file, excluding the header.
-   - `get_page(page, page_size)`: Returns a subset of the dataset corresponding to the given page and page size.
-
-The data is loaded from a CSV file named 'Popular_Baby_Names.csv'. The dataset is cached to optimize repeated access.
-
-Usage:
-    Instantiate the Server class and use its methods to paginate through the dataset.
-"""""
 
 import csv
 import math
@@ -43,10 +30,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(
-            self,
-            page: int = 1,
-            page_size: int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return a page of the dataset based on the page and page size"""
         # Validate input arguments
         assert isinstance(
