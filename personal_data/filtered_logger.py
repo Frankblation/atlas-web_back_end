fb7aa394d0e2be9d572bed42db3e58a42a3c9d2d
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-11 20:40:41 -0500

    sheesh

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""replacing their values with the redaction string."""
+"""Replacing their values with the redaction string."""
 
 from typing import List
+import logging
 import re
 
 
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """
-        Args:
+    Args:
         fields (list): A list of fields to be obfuscated.
         redaction (str): The string that will replace the field's value.
         message (str): The log message to be filtered.
@@ -19,3 +20,26 @@ def filter_datum(fields: List[str],
     """
     return re.sub(f"({'|'.join(fields)})=[^{separator}]*",
                   lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initialize the formatter with a list of fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record by redacting sensitive information.
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
