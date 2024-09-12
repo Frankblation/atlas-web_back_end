1db81b8f65f766f01e2bb969116fb81c6c2ebdf5
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-11 19:56:32 -0500

    retry

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """replacing their values with the redaction string."""
 
-
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
         Args:
         fields (list): A list of fields to be obfuscated.
@@ -16,8 +16,4 @@ def filter_datum(fields, redaction, message, separator):
     Returns:
         str: The filtered log message with the sensitive fields redacted.
     """
-    for field in fields:
-        pattern = f'{field}=.*?{separator}'
-        message = re.sub(pattern, f'{field}={redaction}{separator}', message)
-
-    return message
+    return re.sub(f"({'|'.join(fields)})=[^{separator}]*", lambda m: f"{m.group(1)}={redaction}", message)
