906f8392095009c01e88d408a8c5f5a43d1d0907
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-16 01:53:34 -0500

    documments

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-""" LIFO Cache """
-
+"""Module implementing a LIFO caching system."""
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO caching system """
+    """LIFO caching system that inherits from BaseCaching.
+
+    Implements a cache replacement policy where the last item added
+    is the first one to be discarded when the cache limit is reached.
+    """
 
     def __init__(self):
-        """ Initialize the cache """
+        """Initialize the cache with an additional attribute to track
+        the last inserted key."""
         super().__init__()
-        self.last_key = None  # To keep track of the last inserted key
+        self.last_key = None
 
     def put(self, key, item):
-        """ Assign to the cache the item value for the key key """
+        """Assign the item value to the cache for the given key.
+
+        If the cache exceeds the allowed limit (BaseCaching.MAX_ITEMS),
+        the last added key will be discarded.
+
+        Args:
+            key (str): the key for the cache.
+            item (any): the value to store in the cache.
+        """
         if key is None or item is None:
             return
 
@@ -26,12 +38,18 @@ class LIFOCache(BaseCaching):
         # If cache exceeds the max limit, discard the last added item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             if self.last_key:
-                # Remove the last key that was added
                 print(f"DISCARD: {self.last_key}")
                 del self.cache_data[self.last_key]
 
     def get(self, key):
-        """ Return the value linked to the key """
+        """Return the value linked to the given key.
+
+        Args:
+            key (str): the key to retrieve the value for.
+
+        Returns:
+            any: the value associated with the key, or None if not found.
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
