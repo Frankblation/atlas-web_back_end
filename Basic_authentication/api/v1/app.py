c48ddd2b4dbeaef3ead0ea4b051fc1859c74db1e
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-22 04:07:58 -0500

    yay

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 # Error Handlers
 @app.errorhandler(401)
 def unauthorized_error(error):
