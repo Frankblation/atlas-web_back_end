8afef987f44c12bad5e1d380c1f23527af7104b8
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-22 13:59:50 -0500

    maybe

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,17 @@ class Auth:
         Returns:
             bool: False indicating no authentication is required.
         """
-        return False
+        #if path is none or excluded then requier auth
+        if path is None or not excluded_paths:
+            return True
+
+            # Add trailing slash
+            if path[-1] !='/':
+                path += '/'
+
+            if path in excluded_paths:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
