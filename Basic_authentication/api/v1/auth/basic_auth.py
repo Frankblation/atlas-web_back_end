b1cdb648ffbc51825fb3accd85ca0f6d96e93c79
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-23 08:14:43 -0500

    bassics

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,65 @@ class BasicAuth(Auth):
     For now, this class is empty but will be extended later.
     """
     pass
+
+
+def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
+    """ Function that extracts the base 64 part of the header
+
+        Args:
+            - self
+            - authorization_header: Header to be parsed
+
+        Return:
+            - The base 64 part of the header
+    """
+    # No header is provided
+    if authorization_header is None:
+        return None
+
+    # Header must be a valid string
+    if not isinstance(authorization_header, str):
+        return None
+
+    header = "Basic "
+
+    # Header must start with Basic
+    if authorization_header.startswith(header):
+
+        # Calculate the position at the end of header
+        start_index = len(header)
+        return authorization_header[start_index:]
+    else:
+        return None
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
+        """ Function that extracts the base 64 part of the header
+
+            Args:
+                - self
+                - authorization_header: Header to be parsed
+
+            Return:
+                - The base 64 part of the header
+        """
+    # No header is provided
+    if authorization_header is None:
+        return None
+
+    # Header must be a valid string
+    if not isinstance(authorization_header, str):
+        return None
+
+    header = "Basic "
+
+    # Header must start with Basic
+    if authorization_header.startswith(header):
+
+        # Calculate the position at the end of header
+        start_index = len(header)
+        return authorization_header[start_index:]
+    else:
+        return None
