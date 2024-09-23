b165a4c31adcdc71749b25ed28ea56e5e90db983
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-09-23 08:04:50 -0500

    okay pep8

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -69,6 +69,7 @@ def before_request_func():
     if auth.current_user(request) is None:
         abort(403)  # Forbidden
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5001")
