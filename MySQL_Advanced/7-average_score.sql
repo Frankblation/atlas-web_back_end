2e5665e376e9a63afcf7452511f2adfd335ad36b
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-09 22:15:30 -0500

    okay what?

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,6 @@
---a stored procedure ComputeAverageScoreForUser
---computes and stores the average score for a student
-DELIMITER $$
-
+-- creates a stored procedure ComputeAverageScoreForUser that computes and store the
+-- average score for a student. Note: An average score can be a decimal
+DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
@@ -11,5 +10,5 @@ BEGIN
     UPDATE users
     SET average_score = avg_score
     WHERE users.id = user_id;
-END $$
+END //
 DELIMITER ;
