4f14d769f024af163e3e0efde69452153bacf3df
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-09 22:38:58 -0500

    Revert "okay what?"
    This reverts commit fbfc7a1a2d56cd7ebe49cc4c4621777d00501496.

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,7 @@
--- creates a stored procedure ComputeAverageScoreForUser that computes and store the
--- average score for a student. Note: An average score can be a decimal
-DELIMITER //
+--a stored procedure ComputeAverageScoreForUser
+--computes and stores the average score for a student
+DELIMITER $$
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
@@ -10,5 +11,5 @@ BEGIN
     UPDATE users
     SET average_score = avg_score
     WHERE users.id = user_id;
-END //
+END $$
 DELIMITER ;
