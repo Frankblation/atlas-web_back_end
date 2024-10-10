98d38775a186ff994667871911bf29fc44318d6c
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-10 12:03:43 -0500

    pls work

--- MySQL_Advanced/7-average_score_2.sql
+++ MySQL_Advanced/7-average_score_2.sql
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dedcaed6b0a01225b5719d61e8168bc62863ec4e7ecb5acadaecf47336748f0b
-size 410
+--a stored procedure ComputeAverageScoreForUser
+--computes and stores the average score for a student
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+    UPDATE users
+    SET average_score = avg_score
+    WHERE users.id = user_id;
+END $$
+
+DELIMITER ;
