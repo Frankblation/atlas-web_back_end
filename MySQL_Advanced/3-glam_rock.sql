7c09838f0e5161f661a5f47737fe7276ff061f3e
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-09 21:38:43 -0500

    i'm like 98% sure this is right

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,12 @@
--- Select all bands with Glam rock as their main style, calculating their lifespan
--- Column names must be: band_name and lifespan (in years)
+-- Script to list all bands with Glam rock as their main style, ranked by their longevity
+-- Column names are band_name and lifespan (in years)
+
 SELECT
-    name AS band_name,
-    CASE
-        WHEN split IS NULL THEN YEAR(CURDATE()) - formed  -- if the band hasn't split, calculate lifespan until current year
-        ELSE split - formed  -- if the band has split, calculate lifespan until split year
-    END AS lifespan
+    band_name,
+    IFNULL(YEAR(split), YEAR(CURDATE())) - YEAR(formed) AS lifespan
 FROM
-    bands
+    metal_bands
 WHERE
-    main_style = 'Glam rock'
+    style = 'Glam rock'
 ORDER BY
     lifespan DESC;
