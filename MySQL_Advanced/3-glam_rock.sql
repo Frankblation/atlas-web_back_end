26239e007c753eabaf9c5bbbc5fb68942fcc88cd
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-09 21:48:05 -0500

    spacing

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- Script to list all bands with Glam rock as their main style, ranked by their longevity
 -- Column names are band_name and lifespan (in years)
-
 SELECT band_name, TIMESTAMPDIFF(year, MAKEDATE(formed, 1), IF(split IS NULL, CURDATE(), MAKEDATE(split, 1))) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
