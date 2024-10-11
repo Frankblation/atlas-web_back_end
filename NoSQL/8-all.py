244ec2c81683e9f22fca80c4ad897b85d4611386
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-11 00:11:21 -0500

    okay

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
-"""List all documents in a collection.
 """
+8-all - Module to interact with a
+ MongoDB collection.
+
+This module contains a function to
+list all documents in a specified
+MongoDB collection using PyMongo.
+
+Functions:
+----------
+- list_all(mongo_collection): Returns a
+ list of all documents in the collection.
+"""
+
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
@@ -9,12 +21,12 @@ def list_all(mongo_collection):
     List all documents in a collection.
 
     Parameters:
-    mongo_collection (pymongo.collection.Collection): The collection
-     to list documents from.
+    mongo_collection (pymongo.collection.Collection):
+    The collection to list documents from.
 
     Returns:
-    list: A list of documents in the collection, or an empty list
-     if no documents exist.
+    list: A list of documents in the collection,
+    or an empty list if no documents exist.
     """
     # Fetch all documents from the collection
     documents = list(mongo_collection.find())
