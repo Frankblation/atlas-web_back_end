469a02e99f9ef99b37fef1d89da105d59ca94f58
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-10-24 00:54:27 -0400

    okay maybe

--- unittests_in_js/4-payment.js
+++ unittests_in_js/4-payment.js
@@ -1,8 +1,25 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
-}
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with correct arguments and return 10', function() {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const spy = sinon.spy(console, 'log');
 
-module.exports = sendPaymentRequestToApi;
+    // Call the function
+    sendPaymentRequestToApi(100, 20);
+
+    // Check if Utils.calculateNumber was called with the right arguments
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    
+    // Check if console.log was called once with the expected message
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+
+    // Restore the stub and spy after the test
+    stub.restore();
+    spy.restore();
+  });
+});
